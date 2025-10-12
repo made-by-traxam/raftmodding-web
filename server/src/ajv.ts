a05be132d31b3849d86039d0a23e4c3767842ccb
traxam <traxam@maxschweik.art>
2025-10-12 17:56:36 +0100

    fix: use ajv-errors in server too
    This is necessary to permit the `errorMessage` keyword that we already use.

--- server/src/ajv.ts
+++ server/src/ajv.ts
@@ -1,7 +1,9 @@
 import AJV from 'ajv';
+import ajvErrors from 'ajv-errors'
 
 export const ajv = new AJV({
-  allErrors: true,
+  allErrors: true, // required for 'ajv-errors' which supports custom error messages
   verbose: true,
 });
 ajv.addFormat('time', '^([0-1][0-9]|2[0-3]):[0-5][0-9]$');
+ajvErrors(ajv);
